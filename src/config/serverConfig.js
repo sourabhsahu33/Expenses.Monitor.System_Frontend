944b105e2566ee489d36a9582d070cdb54a15dca
learn github <sourabh.sahu10033@gmail.com>
2024-03-30 22:17:11 +0530

    first commit

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,2 +1,3 @@
-const serverURI = "http://localhost:8080/api/v1";
+// const serverURI = "http://localhost:8080/api/v1";
+const serverURI = "https://expenses-monitor-system-backend.onrender.com/api/v1"
 export default serverURI;
